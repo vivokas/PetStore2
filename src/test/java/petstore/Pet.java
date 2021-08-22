c5708bc54cee3db2453258d288c3a32848fe4009
Viviane <vivianevinici@gmail.com>
2021-08-22 13:01:35 -0300

    Script consultarpet.

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -15,20 +15,20 @@ import static org.hamcrest.Matchers.is;
 
 //3 - Classes
 public class Pet {
-    //3.1 Atributos caracterÃ­sticas
-    String uri = "https://petstore.swagger.io/v2/pet"; //endereÃ§o da entidade
+    //3.1 Atributos características
+    String uri = "https://petstore.swagger.io/v2/pet"; //endereço da entidade
 
-    //3.2 MÃ©todos e FunÃ§Ãµes
+    //3.2 Métodos e Funções
     public String lerJson(String caminhoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(caminhoJson)));
     }
     //Incluir - Create - POst
-    @Test //Identifica o mÃ©todo ou funÃ§Ã£o como um teste para o TestNG
+    @Test(priority = 1)//Identifica o método ou função como um teste para o TestNG
     public void incluirPet() throws IOException {
         String jsonBody = lerJson("db/pet1.json");
 
         //Sintaxe Gherkin
-        //Dado - Quando - EntÃ£o
+        //Dado - Quando - Então
         //Give - When - Then
 
         given()
@@ -43,10 +43,33 @@ public class Pet {
         //checagem se deu certo
                 .body("name", is("Alice"))
                 .body("status",is("available"))
-                .body( "category.name", is("dog"))
-                .body("tags.name", contains("sta"))
+                .body( "category.name", is("NE22CA08LA2021"))
+                .body("tags.name", contains("data"))
         ;
 
+    }
+    //Consultar - Create - POst
+    @Test(priority = 2)
+    public void consultarPet(){
+        String petId = "1210199031";
+        String token =
+
+        given()
+                .contentType("aplication/json")
+                .log().all()
+        .when()
+                .get(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is ("Alice"))
+                .body("category.name", is("NE22CA08LA2021"))
+                .body("status", is("available"))
+        .extract()
+                .path("category.name")
+
+        ;
+        System.out.println("O token é " + token);
     }
 
 }
