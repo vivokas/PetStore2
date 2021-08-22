63eaff3735dc249feef5f363231658ec5df68c2b
Viviane <vivianevinici@gmail.com>
2021-08-22 14:15:19 -0300

    Script excluirpet.

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -72,7 +72,7 @@ public class Pet {
         System.out.println("O token é " + token);
     }
     @Test(priority =3)
-    public void alterarpet() throws IOException {
+    public void alterarPet() throws IOException {
         String jsonBody = lerJson("db/pet2.json");
 
         given()
@@ -87,6 +87,24 @@ public class Pet {
                 .body("name", is("Alice"))
                 .body("status", is("sold"))
 
+        ;
+    }
+
+    @Test(priority = 4)
+    public void excluirPet() {
+        String petId = "1210199031";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is ("unknown"))
+                .body("message", is(petId))
         ;
     }
 }
